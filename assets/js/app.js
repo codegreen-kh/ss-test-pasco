7d8728db61224f2fc925abffd2b3f1b15afaa7e4
codegreen-kh <vitaliy.komisarov@yahoo.com>
2017-01-29 21:57:46 +0200

    Header was done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
 	$(document).foundation();
 	$('.bxslider').bxSlider();
+	$('.main-menu').find('li.menu-item').click(function(){
+		$('.main-menu').find('li.menu-item').removeClass('active-item');
+		$(this).addClass('active-item');
+	});
 });
